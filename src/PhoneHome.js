e1e4200c16dbd71cd07c308af21f0bda46769b10
Tim Ryding <tryding2@gmail.com>
2019-05-05 22:52:42 -0500

    users are added to correct room, no message on wrong room number yet

--- src/PhoneHome.js
+++ src/PhoneHome.js
@@ -1,114 +1,149 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Face from '@material-ui/icons/Face';
-import DialPad from '@material-ui/icons/Dialpad';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
-import {withStyles} from '@material-ui/core'
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Face from "@material-ui/icons/Face";
+import DialPad from "@material-ui/icons/Dialpad";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import {
+  withStyles,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormHelperText
+} from "@material-ui/core";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const styles = theme => ({
   cardItem: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignitems: 'center',
-    padding: '7px',
+    display: "flex",
+    justifyContent: "center",
+    alignitems: "center",
+    padding: "7px"
+  },
+  selections: {
+    minWidth: 120,
+    margin: theme.spacing.unit,
+    dislay: "flex",
+    justifyContent: "center",
+    alignitems: "center"
   }
-})
+});
 
 export default withStyles(styles)(
-class PhoneHome extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      user: '',
-      roomNum: '',
-      games: []
+  class PhoneHome extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        user: "",
+        roomNum: "",
+        selectedGame: "",
+        games: []
+      };
+    }
+
+    async componentDidMount() {
+      const db = firebase.firestore();
+      const dbGames = db.collection("games");
+      await dbGames.get().then(snapshot => {
+        snapshot.forEach(doc => {
+          this.setState({ games: [...this.state.games, doc.data()] });
+        });
+      });
+    }
+
+    handleChange = evt => {
+      this.setState({
+        [evt.target.name]: evt.target.value
+      });
     };
-  }
 
-  async componentDidMount() {
-    let db = firebase.firestore()
-    let dbGames = db.collection('games');
-    await dbGames.get().then(snapshot => {
-      snapshot.forEach(doc => {
-        this.setState({ games: [...this.state.games, doc.data()] })
+    addUser = async () => {
+      const db = firebase.firestore();
+      const { roomNum, user, selectedGame } = this.state;
+      const game = db.collection('games').doc(`${selectedGame}`)
+      let room = game.collection('rooms').doc(`${roomNum}`)
+      room.collection('users').doc(`${user}`).set({
+        name: `${user}`
       })
-    })
-  }
+    };
 
-  handleChange = evt => {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    });
-  };
+    render() {
+      const { games } = this.state;
+      const { classes } = this.props;
 
-  // addUser = () => {
-  //   let db = firebase.firestore();
-  //   let roomNumber = this.state.roomNum;
-  // };
-
-  render() {
-    const { user } = this.state;
-    const { classes } = this.props 
-    
-    return (
-      <div>
-        <header className="header">
-          <img src={require('./logo.png')} alt="logo" />
-        </header>
-        <div
-          style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-          }}
-        >
-          <Card alignitems="center" justify="center">
-            <CardContent>
-              <div className={classes.cardItem}>
-                <Face />
-              </div>
-              <div className={classes.cardItem}>
-                <TextField
-                  id="input-with-icon-grid"
-                  label="Name"
-                  name="user"
-                  value={this.state.user}
+      return (
+        <div>
+          <header className="header">
+            <img src={require("./logo.png")} alt="logo" />
+          </header>
+          <div
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)"
+            }}
+          >
+            <Card alignitems="center" justify="center">
+              <CardContent>
+                <div className={classes.cardItem}>
+                  <Face />
+                </div>
+                <div className={classes.cardItem}>
+                  <TextField
+                    id="input-with-icon-grid"
+                    label="Name"
+                    name="user"
+                    value={this.state.user}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className={classes.cardItem}>
+                  <DialPad />
+                </div>
+                <div className={classes.cardItem}>
+                  <TextField
+                    id="input-with-icon-grid"
+                    label="Room Code"
+                    name="roomNum"
+                    value={this.state.roomNum}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <Select
                   onChange={this.handleChange}
-                />
-              </div>
-              <div className={classes.cardItem}>
-                <DialPad />
-              </div>
-              <div className={classes.cardItem}>
-                <TextField
-                  id="input-with-icon-grid"
-                  label="Room Code"
-                  name="roomNum"
-                  value={this.state.roomNum}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </CardContent>
-            <CardActions>
-              <Button
-                size="large"
-                color="primary"
-                fullWidth={true}
-                // onClick={() => {
-                //   this; }}
-              >
-                Submit
-              </Button>
-            </CardActions>
-          </Card>
+                  value={this.state.selectedGame}
+                  inputProps={{ name: "selectedGame" }}
+                  className={classes.selections}
+                >
+                  <InputLabel>Choose Game</InputLabel>
+                  <MenuItem value="">
+                    <em>Choose a game</em>
+                  </MenuItem>
+                  {games.map(game => (
+                    <MenuItem value={game.name} key={game.name}>
+                      {game.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="large"
+                  color="primary"
+                  fullWidth={true}
+                  onClick={this.addUser}
+                >
+                  Submit
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-})
+);
