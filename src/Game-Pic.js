9f301e46d7cb7cf5e0109174acd2beab2b6c8ed6
Christa Kaspo <christa.kaspo@gmail.com>
2019-05-06 14:40:34 -0500

    WE CAN CLEAR THE DRAWING PADgit add .git add .git add .git add . :D

--- src/Game-Pic.js
+++ src/Game-Pic.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import Button from '@material-ui/core/Button';
+import { clear } from 'google-auth-library/build/src/auth/envDetect';
 
-const PictionaryHome = () => {
-  return (
-    <div>
-      <CanvasDraw
-        hideGrid="true"
-        brushRadius="3"
-        canvasHeight="70vh"
-        canvasWidth="100vw"
-        lazyRadius="10"
-        brushColor=" #000000"
-        style={{
-          boxShadow:
-            '0 13px 27px -5px rgba(20, 20, 63, 0.1),    0 2px 3px -2px rgba(1, 200, 1, 0.3)',
-        }}
-      />
-      <Button onClick={() => CanvasDraw.clear()}>Clear</Button>
-      <Button size="medium" color="primary" fullWidth={true}>
-        Submit
-      </Button>
-    </div>
-  );
-};
+class PictionaryHome extends React.Component {
+  render() {
+    return (
+      <div>
+        <CanvasDraw
+          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+          hideGrid={true}
+          brushRadius={3}
+          lazyRadius={10}
+          brushColor="#000000"
+          style={{
+            boxShadow:
+              '0 13px 27px -5px rgba(20, 20, 63, 0.1),    0 2px 3px -2px rgba(1, 200, 1, 0.3)',
+          }}
+        />
+        <Button
+          size="medium"
+          color="primary"
+          fullWidth={true}
+          onClick={() => {
+            this.saveableCanvas.clear();
+          }}
+        >
+          Clear
+        </Button>
+        <Button size="medium" color="primary" fullWidth={true}>
+          Submit
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default PictionaryHome;
