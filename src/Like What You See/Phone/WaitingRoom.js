8fe56a5a3e9dc0f5c02edf46bfeec683b4b1e469
Tim Ryding <tryding2@gmail.com>
2019-05-08 10:04:29 -0500

    user object gets grabbed from database in waitingroom (#41)

--- src/Like What You See/Phone/WaitingRoom.js
+++ src/Like What You See/Phone/WaitingRoom.js
@@ -1,26 +1,35 @@
 import React from "react";
+import { db } from "../../index";
 
 export class WaitingRoom extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            roomNum: 0,
-            game: {},
-            user: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      roomNum: 0,
+      game: {},
+      user: {}
+    };
+  }
 
-    componentDidMount() {
-        const {roomNum, game, user} = this.props.location.state
-        this.setState({ roomNum, game, user})
-    }
+  componentDidMount() {
+    const { roomNum, game, user } = this.props.location.state;
+    let currentPlayer = db
+      .collection("games")
+      .doc(`${game.name}`)
+      .collection("rooms")
+      .doc(`${roomNum}`)
+      .collection("users")
+      .doc(`${user}`);
+    this.setState({ roomNum, game, user: currentPlayer });
+  }
 
-    componentWillUpdate() {
-        console.log(this.state, this.props)
-    }
+  componentWillUpdate() {
+    console.log(this.state, this.props);
+  }
 
   render() {
-      const {roomNum, game, user} = this.state
+    console.log(this.state)
+    const { roomNum, game, user } = this.state;
     return (
       <div>
         <h1>inside the waiting room!</h1>
