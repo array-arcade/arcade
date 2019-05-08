6d883445d8165393ea8adf3f3ce34d2a4cd10832
Brett E Stockmeier <stockmeierb@gmail.com>
2019-05-08 10:44:15 -0500

    consolidated error messages

--- src/PhoneHome.js
+++ src/PhoneHome.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Face from '@material-ui/icons/Face';
-import DialPad from '@material-ui/icons/Dialpad';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Face from "@material-ui/icons/Face";
+import DialPad from "@material-ui/icons/Dialpad";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
 import {
   withStyles,
@@ -47,7 +47,7 @@ export default withStyles(styles)(
         selectedGame: "none",
         games: [],
         error: false,
-        full: false
+        message: ""
       };
     }
     targetElement = null;
@@ -104,18 +104,26 @@ export default withStyles(styles)(
                 .set({
                   name: `${user}`
                 });
+
+              return this.props.history.push({
+                pathname: `/${roomNum}/waitingroom`,
+                state: { roomNum, game, user }
+              });
             } else {
               //render code indicating room is full
-              this.setState({ full: true });
-              console.log("Room is full logic: 107");
+              this.setState({
+                error: true,
+                message:
+                  "The selected room is full currently and you will not be able to join."
+              });
             }
           } else {
-            this.setState({ error: true });
+            this.setState({
+              error: true,
+              message:
+                "An error has occured. Make sure you have a name and a valid room number for the game you are trying to play."
+            });
           }
-          return this.props.history.push({
-            pathname: `/${roomNum}/waitingroom`,
-            state: { roomNum, game, user }
-          });
         })
         .catch(err => console.log("Something went wrong!", err));
     };
@@ -195,29 +203,13 @@ export default withStyles(styles)(
                 >
                   <DialogContent>
                     <DialogContentText>
-                      An error has occured. Make sure you have a name and a
-                      valid room number for the game you are trying to play.
+                      <DialogContentText>
+                        {this.state.message}
+                      </DialogContentText>
                     </DialogContentText>
                   </DialogContent>
                   <DialogActions>
                     <Button onClick={() => this.setState({ error: false })}>
-                      Close
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-                {/* added another error dialogue for case when room is full */}
-                <Dialog
-                  open={this.state.full}
-                  onClose={() => this.setState({ full: false })}
-                >
-                  <DialogContent>
-                    <DialogContentText>
-                      This game is already full, and you are unable to join at
-                      this time.
-                    </DialogContentText>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={() => this.setState({ full: false })}>
                       Close
                     </Button>
                   </DialogActions>
