ae645ad66728593099e78f9327ecb234cccdb59f
Tim Ryding <tryding2@gmail.com>
2019-05-10 11:33:02 -0500

    please god

--- src/Like What You See/Browser/PictureDisplays.js
+++ src/Like What You See/Browser/PictureDisplays.js
@@ -73,7 +73,7 @@ export default withStyles(styles)(
     }
 
     render() {
-      let { players } = this.state;
+      let { players, roomNumber } = this.state;
       let { classes } = this.props;
 
       return (
